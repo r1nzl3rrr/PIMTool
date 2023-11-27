eb9ccf4c164779320d8077a7c236e1e13e5652d0
r1nzl3rrr <khanhle9344@gmail.com>
2023-11-28 00:15:51 +0700

    Updated Core Component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ManagerModule } from './manager/manager.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CoreModule,
+    ManagerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
